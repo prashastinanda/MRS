953b08550e753ed91517c58898e1878531fc8216
prashastinanda <139529682+prashastinanda@users.noreply.github.com>
2025-04-05 23:15:26 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    darkMode: 'class', // ← this line is essential
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
